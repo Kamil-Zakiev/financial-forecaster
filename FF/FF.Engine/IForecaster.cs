5cd241066c2eb3fdad286929f6837f3395cb0e1e
Kamil Zakiev <zakiev-kamil@mail.ru>
2021-05-29 21:19:38 +0300

    started forecaster

---
+++ FF/FF.Engine/IForecaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FF.Engine
+{
+    public interface IForecaster
+    {
+        IReadOnlyCollection<Forecast> Forecast(IAccountant accountant, DateTime from, DateTime to);
+    }
+
+    internal class Forecaster : IForecaster
+    {
+        public IReadOnlyCollection<Forecast> Forecast(IAccountant accountant, DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Forecast
+    {
+    }
+}
